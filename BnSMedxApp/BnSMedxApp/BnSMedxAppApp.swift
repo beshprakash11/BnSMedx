904b342ffd605aabd2e2450b85f9ee3b8ee64d06
Besh Prakash Yogi <beshprakashktm@gmail.com>
2026-02-17 17:29:42 +0100

    App added

---
+++ BnSMedxApp/BnSMedxApp/BnSMedxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BnSMedxAppApp.swift
+//  BnSMedxApp
+//
+//  Created by Besh Prakash Yogi on 17.02.26.
+//
+
+import SwiftUI
+
+@main
+struct BnSMedxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
